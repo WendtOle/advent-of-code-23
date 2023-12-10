6f117375e62c0fa90d6446d5bd43e39cd90a4c45
Olee <olee@Oles-MacBook-Air.local>
2023-12-10 09:12:11 +0100

    init ts project with tests

---
+++ src/walkingRange.ts
@@ -0,0 +1,5 @@
+export const walkingRange = () => {
+    return -1
+}
+
+console.log("hello world")
---
+++ test/walkingRage.spec.ts
@@ -0,0 +1,9 @@
+import { expect } from "chai";
+import { walkingRange } from "../src/walkingRange";
+
+describe("walking range", () => {
+    it("should handle first example", () => {
+        expect(walkingRange()).to.equal(0);
+    }
+    )
+})
