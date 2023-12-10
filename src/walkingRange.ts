c116cadd811f8579a886deae8b097c6396035639
Olee <olee@Oles-Air.fritz.box>
2023-12-10 11:12:00 +0100

    Implement walking the circle

--- src/walkingRange.ts
+++ src/walkingRange.ts
@@ -1,3 +1,39 @@
+import { connectedTiles } from "./connectedTiles"
+import { loadAreaMap } from "./loadAreaMap"
+import { startPos } from "./startPos"
+import { AreaMap, Position } from "./types"
+
 export const walkingRange = (mapFileName: string) => {
-    return -1
+    const areaMap = loadAreaMap(mapFileName)
+    const paths = getPaths(areaMap)
+    return (paths[0].length - 1) / 2
+}
+
+const isPositionSame = (left: Position, right: Position) => {
+    return left.x === right.x && left.y === right.y
+}
+
+const getPaths = (areaMap: AreaMap) => {
+    const startPosition = startPos(areaMap)
+    const connected = connectedTiles(areaMap, startPosition)
+    const output = connected.map((position) => {
+        let current: Position = position
+        const path = [startPosition, current]
+        while (!isPositionSame(current, startPosition)) {
+            const next = getNext(areaMap, path[path.length - 2], current)
+            current = next
+            path.push(current)
+        }
+        return path
+    })
+    return output
+}
+
+const getNext = (areaMap: AreaMap, prev: Position, curr: Position): Position => {
+    const connected = connectedTiles(areaMap, curr)
+    const filtered = connected.filter((potentialNext) => !isPositionSame(potentialNext, prev))
+    if (filtered.length !== 1) {
+        throw new Error("There is not only one path to follow")
+    }
+    return filtered[0]
 }
--- test/walkingRage.spec.ts
+++ test/walkingRage.spec.ts
@@ -3,19 +3,23 @@ import { walkingRange } from "../src/walkingRange";
 
 describe("walking range", () => {
     it("should handle first example", () => {
-        expect(walkingRange("example-1")).to.equal(4);
+        expect(walkingRange("data/example-1")).to.equal(4);
     }
     ),
-    it("should handle first example", () => {
-        expect(walkingRange("example-2")).to.equal(4);
+    it("should handle second example", () => {
+        expect(walkingRange("data/example-2")).to.equal(4);
     }
     ),
-    it("should handle first example", () => {
-        expect(walkingRange("example-3")).to.equal(8);
+    it("should handle third example", () => {
+        expect(walkingRange("data/example-3")).to.equal(8);
     }
     ),
-    it("should handle first example", () => {
-        expect(walkingRange("example-4")).to.equal(8);
+    it("should handle fourth example", () => {
+        expect(walkingRange("data/example-4")).to.equal(8);
+    }
+    )
+    it("should handle fifth example", () => {
+        expect(walkingRange("data/example-5")).to.equal(6897);
     }
     )
 })
