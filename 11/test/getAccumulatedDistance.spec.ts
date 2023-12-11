095b1e52dd15d29871b8b82ad88a9405c2ce0ffc
Olee <olee@Oles-Air.fritz.box>
2023-12-11 19:14:07 +0100

    Add test cases which scale the image by 10, 100 and one million

--- 11/test/getAccumulatedDistance.spec.ts
+++ 11/test/getAccumulatedDistance.spec.ts
@@ -18,4 +18,19 @@ describe("getAccumulatedDistance", () => {
         const actual = getAccumulatedDistance(input)
         expect(toDistance(actual,2)).to.equal(9795148)
     })
+    it("returns accumulated distance for first example with factor 10", () => {
+        const input = loadImage("11/data/example-1")
+        const actual = getAccumulatedDistance(input)
+        expect(toDistance(actual,10)).to.equal(1030)
+    }) 
+    it("returns accumulated distance for first example with factor 100", () => {
+        const input = loadImage("11/data/example-1")
+        const actual = getAccumulatedDistance(input)
+        expect(toDistance(actual,100)).to.equal(8410)
+    })
+    it("returns accumulated distance for actual puzzle", () => {
+        const input = loadImage("11/data/puzzle")
+        const actual = getAccumulatedDistance(input)
+        expect(toDistance(actual,1000000)).to.equal(650672493820)
+    })
 })
