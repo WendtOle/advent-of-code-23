3f62ccbc7518ce42d38cdbee67491d7de136ceab
Olee <olee@Oles-Air.fritz.box>
2023-12-11 18:26:58 +0100

    Implement getDistance()

--- 11/src/getDistance.ts
+++ 11/src/getDistance.ts
@@ -1,5 +1,10 @@
-import { Image } from "./types";
+import { Position } from "./types";
 
-export const getDistance = (image: Image, first: number, second: number): number => {
-    return 0
+export const getDistance = (firstPosition: Position, secondPosition: Position): number => {
+    const {x: x1, y: y1}: Position = firstPosition;
+    const {x: x2, y: y2}:  Position = secondPosition;
+    const verticalDistance = Math.abs(y1 - y2)
+    const horizontalDistance = Math.abs(x1 - x2)
+    //console.log({x1, x2, y1, y2})
+    return verticalDistance + horizontalDistance
 }
--- 11/test/getDistance.spec.ts
+++ 11/test/getDistance.spec.ts
@@ -1,26 +1,25 @@
 import { expect } from "chai"
 import { loadImage } from "../src/loadImage"
 import { getDistance } from "../src/getDistance"
+import { getGalaxyPositions } from "../src/getGalaxyPositions"
+
+const galaxyPositions = getGalaxyPositions(loadImage("11/data/example-1-expanded"))
 
 describe("getDistance", () => {
     it("returns given distance between galaxy 5 and 9", () => {
-        const input = loadImage("11/data/example-1-expanded")
-        const actual = getDistance(input, 5, 9)
+        const actual = getDistance(galaxyPositions[5 - 1], galaxyPositions[9 - 1])
         expect(actual).to.equal(9)
     })
     it("returns given distance between galaxy 1 and 7", () => {
-        const input = loadImage("11/data/example-1-expanded")
-        const actual = getDistance(input, 1, 5)
+        const actual = getDistance(galaxyPositions[1 - 1], galaxyPositions[7 - 1])
         expect(actual).to.equal(15)
     })
     it("returns given distance between galaxy 3 and 6", () => {
-        const input = loadImage("11/data/example-1-expanded")
-        const actual = getDistance(input, 3, 6)
+        const actual = getDistance(galaxyPositions[3 - 1], galaxyPositions[6 - 1])
         expect(actual).to.equal(17)
     })
     it("returns given distance between galaxy 8 and 9", () => {
-        const input = loadImage("11/data/example-1-expanded")
-        const actual = getDistance(input, 8, 9)
+        const actual = getDistance(galaxyPositions[8 - 1], galaxyPositions[9 - 1])
         expect(actual).to.equal(5)
     })
 })
