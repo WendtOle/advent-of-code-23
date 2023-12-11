755b46933e82d59f02420b7553b2dbdc9c52ca67
Olee <olee@Oles-Air.fritz.box>
2023-12-11 18:26:43 +0100

    Implement getGalaxies()

--- 11/src/getGalaxyPositions.ts
+++ 11/src/getGalaxyPositions.ts
@@ -1,6 +1,6 @@
-import { Image, SpaceType } from "./types";
+import { Image, Position, SpaceType, notUndefined } from "./types";
 
-export const getGalaxyPositions = (image: Image) => 
+export const getGalaxyPositions = (image: Image): Position[] => 
     image
         .flatMap((row, y) => row.map((spaceType, x) => spaceType === SpaceType.GALAXY ? ({ x, y }) : undefined))
-        .filter((position) => position !== undefined)
+        .filter(notUndefined)
--- 11/src/types.ts
+++ 11/src/types.ts
@@ -9,3 +9,10 @@ export const spaceTypeMap: Record<string, SpaceType> = {
     ".": SpaceType.EMPTY,
     "#": SpaceType.GALAXY
 }
+
+export interface Position {
+    x: number;
+    y: number;
+}
+
+export const notUndefined = <T>(input: T | undefined): input is T => input !== undefined
