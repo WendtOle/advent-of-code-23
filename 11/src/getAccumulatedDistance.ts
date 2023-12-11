72413b30367d284fba42f1acca8a5bc21de429f4
Olee <olee@Oles-Air.fritz.box>
2023-12-11 18:27:07 +0100

    Implement getAccumulatedDistance()

--- 11/src/getAccumulatedDistance.ts
+++ 11/src/getAccumulatedDistance.ts
@@ -1,5 +1,19 @@
+import { getDistance } from "./getDistance";
+import { getExpanded } from "./getExpanded";
+import { getGalaxyPositions } from "./getGalaxyPositions";
 import { Image } from "./types";
 
 export const getAccumulatedDistance = (image: Image): number => {
-    return 0
+    const expanded = getExpanded(image)
+    const galaxyPositions = getGalaxyPositions(expanded)
+    let distance = 0 
+    let counter = 0
+    for(let i = 0; i < galaxyPositions.length; i++) {
+        for(let j = 0; j < i; j++) {
+            counter ++
+            const currentDistance = getDistance(galaxyPositions[i], galaxyPositions[j])
+            distance += currentDistance
+        }
+    }
+    return distance
 }
--- 11/test/getAccumulatedDistance.spec.ts
+++ 11/test/getAccumulatedDistance.spec.ts
@@ -4,13 +4,13 @@ import { getAccumulatedDistance } from "../src/getAccumulatedDistance"
 
 describe("getAccumulatedDistance", () => {
     it("returns accumulated distance for first example", () => {
-        const input = loadImage("11/data/example-1-expanded")
+        const input = loadImage("11/data/example-1")
         const actual = getAccumulatedDistance(input)
-        expect(actual).to.equal(135)
+        expect(actual).to.equal(374)
     })
     it("returns accumulated distance for actual puzzle", () => {
         const input = loadImage("11/data/puzzle")
         const actual = getAccumulatedDistance(input)
-        expect(actual).to.equal(-1)
+        expect(actual).to.equal(9795148)
     })
 })
